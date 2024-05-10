6da54635d1406f35f507e4c41294b32b6890f358
Moetez Welhazi <welhazi.moetez@gmail.com>
2024-05-11 00:18:53 +0100

    fixed mapper(?)

--- src/main/java/com/webproject/ecommerce/mappers/ProductOrderMapper.java
+++ src/main/java/com/webproject/ecommerce/mappers/ProductOrderMapper.java
@@ -4,6 +4,7 @@ import com.webproject.ecommerce.dto.ProductOrderDTO;
 import com.webproject.ecommerce.entities.Invoice;
 import com.webproject.ecommerce.entities.ProductOrder;
 import com.webproject.ecommerce.entities.User;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
@@ -11,7 +12,19 @@ import org.mapstruct.Named;
 @Mapper(componentModel = "spring")
 public interface ProductOrderMapper {
 
-    ProductOrderDTO toDto(ProductOrder productOrder, String message);
+    @Mapping(target = "customer_id", source = "customer", qualifiedByName = "mapToUserId")
+    @Mapping(target = "invoice_id", source = "invoice", qualifiedByName = "mapToInvoiceId")
+    ProductOrderDTO toDto(ProductOrder productOrder, @Context String message);
+
+    @Named("mapToUserId")
+    default Long mapToUserId(User customer){
+        return customer.getId();
+    }
+
+    @Named("mapToInvoiceId")
+    default Long mapToInvoiceId(Invoice invoice){
+        return invoice.getId();
+    }
 
     @Mapping(target = "customer", source = "customer_id", qualifiedByName = "mapToUser")
     @Mapping(target = "invoice", source = "invoice_id", qualifiedByName = "mapToInvoice")
