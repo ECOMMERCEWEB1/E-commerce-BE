1453a474aa4e27d8b0bad81be24ed1977ad0985e
Moetez Welhazi <welhazi.moetez@gmail.com>
2024-05-10 23:29:21 +0100

    Added ProductDTO

--- src/main/java/com/webproject/ecommerce/dto/ProductOrderDTO.java
+++ src/main/java/com/webproject/ecommerce/dto/ProductOrderDTO.java
@@ -1,14 +1,24 @@
 package com.webproject.ecommerce.dto;
 
-import com.webproject.ecommerce.entities.ProductOrder;
+import com.webproject.ecommerce.entities.OrderItem;
+import java.time.Instant;
+import com.webproject.ecommerce.enums.OrderStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class ProductOrderDTO {
-    private ProductOrder productOrder;
+    private Set<OrderItem> orderItems;
     private String message;
+    private Long cutomer_id;
+    private String code;
+    private OrderStatus status;
+    private Instant placedDate;
+    private Long invoice_id;
+
 }
